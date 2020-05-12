4491de602fae4527796309089bfd27fd3f288229
lqj679ssn <lqj679ssn@qq.com>
2020-05-12 11:19:59 +0800

    allow auth

--- Base/common.py
+++ Base/common.py
@@ -2,7 +2,13 @@ from SmartDjango import NetPacker
 
 from Config.models import Config, CI
 
+
+def data_packer(resp):
+    return resp['body']
+
+
 NetPacker.set_mode(debug=True)
+NetPacker.customize_data_packer(data_packer)
 
 WX_TOKEN = Config.get_value_by_key(CI.WX_TOKEN)
 WX_AES_KEY = Config.get_value_by_key(CI.WX_AES_KEY)
--- wechat/views.py
+++ wechat/views.py
@@ -1,8 +1,21 @@
-from django.http import HttpRequest
+from SmartDjango import Analyse
 from django.views import View
+from smartify import P
+from wechatpy.utils import check_signature
+
+from Base.common import WX_TOKEN
 
 
 class MessageView(View):
     @staticmethod
-    def get(r: HttpRequest):
-        print(r.GET.dict())
+    @Analyse.r(q=[
+        P('signature').validate(str),
+        P('timestamp').validate(str),
+        P('nonce').validate(str),
+        P('echostr').validate(str)])
+    def get(r):
+        try:
+            check_signature(token=WX_TOKEN, **r.d.dict('signature', 'timestamp', 'nonce'))
+            return r.d.echostr
+        except Exception:
+            pass
