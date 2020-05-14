23d15429f572b0bf7da29551c72e5d735325e491
lqj679ssn <lqj679ssn@qq.com>
2020-05-14 16:25:21 +0800

    test tuwen

--- wechat/views.py
+++ wechat/views.py
@@ -1,10 +1,12 @@
 import datetime
+from typing import Union
 
 from SmartDjango import Analyse, E
 from django.views import View
 from wechatpy import parse_message
-from wechatpy.messages import TextMessage
-from wechatpy.replies import TextReply
+from wechatpy.events import BaseEvent
+from wechatpy.messages import TextMessage, BaseMessage
+from wechatpy.replies import TextReply, ArticlesReply
 
 from Base.auth import Auth
 from Base.common import wechat_client
@@ -25,10 +27,21 @@ class MessageView(View):
     @Auth.wechat
     def post(r):
         user = r.d.user
-        print(r.body)
-        message = parse_message(r.body)
+        message = parse_message(r.body)  # type: Union[BaseMessage, BaseEvent]
 
-        if message.type == 'text':
+        if message.type == 'event':
+            if message.event.startswith('subscribe'):
+                reply = ArticlesReply()
+                reply.add_article(dict(
+                    title='极客 # 如何上手MasterWhole',
+                    description='我不是订阅号，我是工具箱。',
+                    image='https://res.6-79.cn/disk/cover/WSp5/1574054229.979414/FullSizeRender.jpg?e=1589448273&token=oX6jJmjudP-3BXHJ3A8lYjEQRlQHBc70734ZyTR4:1kQHVWcGdJ82DAwhB3LSqB3uzDQ=',
+                    url='https://d.6-79.cn/res/SgARFB',
+                ))
+                return reply
+            else:
+                content = '暂不支持回复非文字消息'
+        elif message.type == 'text':
             try:
                 content = MessageHandler(user, message.content).message
             except E as e:
