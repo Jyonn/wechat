cdd638799e604630dfe593d748aa9bedc508faba
lqj679ssn <lqj679ssn@qq.com>
2020-05-27 13:23:41 +0800

    cross-domain

--- wechat/settings.py
+++ wechat/settings.py
@@ -54,6 +54,37 @@ MIDDLEWARE = [
     'SmartDjango.middleware.HttpPackMiddleware',
 ]
 
+# 增加跨域
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    # 'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Pragma',
+    'Token',
+)
+
 ROOT_URLCONF = 'wechat.urls'
 
 TEMPLATES = [
